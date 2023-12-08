999eb5c393962308e42f21e28ab1ccb012ab5054
Ramanand Thakur <ramanandt35@gmail.com>
2023-12-09 01:12:04 +0530

    made motion div CSR keeping rest as SSR component

--- components/AnimeCard.tsx
+++ components/AnimeCard.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { MotionDiv } from "./MotionDiv";
 
 export interface AnimeProp {
   id: string;
@@ -16,10 +17,29 @@ interface Prop {
   anime: AnimeProp;
   index: number;
 }
+const varients = {
+  hidden: { opacity: 0 },
+  visible: { opacity: 1 },
+
+
+}
+
 
 function AnimeCard({ anime }: Prop) {
   return (
-    <div className="max-w-sm rounded relative w-full">
+    <MotionDiv
+      variants={varients}
+      initial="hidden"
+      animate="visible"
+      transition={{
+        delay: 1,
+        ease: "easeInOut",
+        duration: 0.5,
+      }}
+      viewport={{ amount: 0 }}
+
+
+      className="max-w-sm rounded relative w-full">
       <div className="relative w-full h-[37vh]">
         <Image
           src={`https://shikimori.one${anime.image.original}`}
@@ -64,7 +84,7 @@ function AnimeCard({ anime }: Prop) {
           </div>
         </div>
       </div>
-    </div>
+    </MotionDiv>
   );
 }
 
